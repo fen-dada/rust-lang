3ea37db97e929fc050e4f055e32aacaebc615264
fen-dada <1823215739@qq.com>
2023-08-21 19:04:27 +0800

    .

--- hehe/target/debug/deps/hehe-712c5911963d3843.d
+++
@@ -1,5 +0,0 @@
-/home/Fendada/rust-lang/hehe/target/debug/deps/hehe-712c5911963d3843.rmeta: src/main.rs
-
-/home/Fendada/rust-lang/hehe/target/debug/deps/hehe-712c5911963d3843.d: src/main.rs
-
-src/main.rs:
--- hehe/target/debug/hehe.d
+++
@@ -1,1 +0,0 @@
-/home/Fendada/rust-lang/hehe/target/debug/hehe: /home/Fendada/rust-lang/hehe/src/main.rs
