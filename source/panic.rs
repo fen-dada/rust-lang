0adc46e94aa63c7b3d0c266787890479c465f70e
fen-dada <1823215739@qq.com>
2023-09-02 14:48:52 +0800

    hehe

--- source/panic.rs
+++ source/panic.rs
@@ -1,5 +1,24 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io::{self,Read};
+//let mut username = String::new();
+//File::open("hello.txt")?.read_to_string(&mut username)?;
+//Ok(username);
+fn read_name_from_file() -> Result<String,io::Error> { // fs::read_to_string("hello.txt")
+    let file_result = File::open("hello.txt");
+
+    let mut file_open = match file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),//its a Result type of the whole function
+    };
+
+    let mut username = String::new();
+
+    match file_open.read_to_string(&mut username) { //borrow a mutable reference to username
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
 fn main() {
     //    panic!("nmsl");
     let file_open_result = File::open("hello.txt");
@@ -16,4 +35,5 @@ fn main() {
             other_error => panic!("Error opening file {:?}", other_error),
         },
     };
+    read_name_from_file();
 }
