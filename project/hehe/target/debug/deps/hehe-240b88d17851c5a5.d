972f8e7ecbbc99e0110777d794422904e6c6c7e0
fen-dada <1823215739@qq.com>
2023-10-02 16:05:26 +0800

    hehe

---
+++ project/hehe/target/debug/deps/hehe-240b88d17851c5a5.d
@@ -0,0 +1,5 @@
+/home/a123456/rust-lang/project/hehe/target/debug/deps/hehe-240b88d17851c5a5.rmeta: src/main.rs
+
+/home/a123456/rust-lang/project/hehe/target/debug/deps/hehe-240b88d17851c5a5.d: src/main.rs
+
+src/main.rs:
---
+++ project/hehe/target/debug/deps/hehe-54271cdcf4c03cf2.d
@@ -0,0 +1,5 @@
+/home/a123456/rust-lang/project/hehe/target/debug/deps/hehe-54271cdcf4c03cf2.rmeta: src/main.rs
+
+/home/a123456/rust-lang/project/hehe/target/debug/deps/hehe-54271cdcf4c03cf2.d: src/main.rs
+
+src/main.rs:
